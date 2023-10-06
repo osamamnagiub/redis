cde7effb85d1400b44ad4700495821cf9f27cadc
osama <onagiub@wordbee.com>
2023-10-06 14:05:33 +0300

    readArray and readBulk impl

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"os"
 )
 
 func main() {
@@ -25,17 +23,16 @@ func main() {
 
 	defer conn.Close()
 	for {
-		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
+		resp := NewResp(conn)
+		value, err := resp.Read()
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
-
 			fmt.Println("error reading from client: ", err.Error())
-			os.Exit(1)
+			return
 		}
 
+		fmt.Println(value)
+
+		// ignore request and send back a PONG
 		conn.Write([]byte("+OK\r\n"))
 	}
 }
